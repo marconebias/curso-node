6fd4b535c0e1a02d97713de5e6f3fcd74d1c6d4a
Marco Nébias <marconebias@hotmail.com>
2023-08-21 19:41:12 -0300

    Primeira mudança

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ const app = express();
 
 
 app.listen(8080, function(){
-    console.log("Servidor Funcionando bem !!! url http//localhost:8080")
+    console.log("Servidor Funcionando muito bem !!! url http//localhost:8080")
 })
