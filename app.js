c9eb6e563df868ce637788d75c97c1059fcec2e4
Marco Nébias <marconebias@hotmail.com>
2023-08-21 23:18:14 -0300

    30 % curso

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
-const express = require("express")
-
+const express = require("express");
+const { Sequelize } = require("sequelize");
 const app = express();
+const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
 
+//Configurações
+    //Template Engine
+        app.engine('handlebars', handlebars.engine({
+            defaultLayout: 'main'
+        }))
+        app.set('view engine', 'handlebars')
+
+//Body-Parser Configure
+        app.use(bodyParser.urlencoded({extended: false}))
+        app.use(bodyParser.json())
+
+
+//Conexão com O MySql
+const sequelize = new Sequelize('test', 'root', '123456', {
+    host: "localhost",
+    dialect: 'mysql'
+})
+
+//Rotas
+app.get('/cad', function(req, res){
+    res.render('formulario')
+})
+
+app.post('/add', function(req, res){
+    
+    res.send('Texto: '+req.body.titulo+" Conteúdo: "+req.body.conteudo)
+})
 
 
 app.listen(8080, function(){
